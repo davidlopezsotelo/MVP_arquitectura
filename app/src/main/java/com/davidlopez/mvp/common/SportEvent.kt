628ed6198b30eb753f4a255dafa2da18a4050d27
davidlopezsotelo <davidaquarium@yahoo.es>
2024-07-08 13:37:13 +0200

    fin

--- app/src/main/java/com/davidlopez/mvp/common/SportEvent.kt
+++ app/src/main/java/com/davidlopez/mvp/common/SportEvent.kt
@@ -24,7 +24,7 @@ sealed class SportEvent {
 
     data object AdEvent: SportEvent()
     data object SaveEvent: SportEvent()
-    data object CloseEvent: SportEvent()
+   // data object CloseEvent: SportEvent()
 
     data object ClosedAdEvent: SportEvent()
 }
